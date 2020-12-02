c6475d7351aa3b1c288f1ad7166b6ca051289543
Jonas Brunhage <jonas.brunhage@gmail.com>
2020-12-02 13:48:36 +0100

    add logout

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -9,11 +9,30 @@ import { UserContext } from '../../shared/provider/UserProvider'
 export const NavBar = () => {
     const history = useHistory()
     const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+
+    const logout = () => {
+        setLoggedInUser(false)
+    }
+
+    const displayUser = () => {
+        if (loggedInUser) {
+            return <div className="profileName">
+                <span >{loggedInUser}</span>
+                <div className="logout" onClick={() => logout()}>Logout</div>
+            </div>
+
+        }
+        else {
+            return <span className="signIn" onClick={() => history.push('/signin')}>Sign in</span>
+        }
+
+    }
+
     return (
         <div className="navBarWrapper">
             <img onClick={() => history.push("/home")} className="logo" src={Logo} alt={"Error"} />
-            <span className="signIn" onClick={() => history.push('/signin')}>Sign in</span>
-            <span>{loggedInUser}</span>
+
+            {displayUser()}
         </div>
     )
 }
