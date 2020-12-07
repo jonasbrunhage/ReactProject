3dd31168f600145871bf306870eb0241fab70d94
Jonas Brunhage <jonas.brunhage@gmail.com>
2020-12-07 15:12:37 +0100

    add poke search

--- src/view/PokemonView.js
+++ src/view/PokemonView.js
@@ -4,6 +4,9 @@ import Axios from 'axios'
 export const PokemonView = () => {
 
     const [pokemonList, setPokemonList] = useState([])
+    const [search, setSearch] = useState("")
+    const [searchCharacters, setSearchCharacter] = useState("")
+    const [searchedPokemon, setSearchedPokemon] = useState({})
 
     const fetchDataFromPokemonAPI = () => {
 
@@ -14,14 +17,37 @@ export const PokemonView = () => {
             .catch((error) => { console.log(error) })
 
     }
+    const fetchDataFromPokemonSearch = () => {
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`)
+            .then((response) => { setSearchedPokemon(response.data) })
+            .catch((error) => { console.log(error) })
+    }
+
     useEffect(() => {
         fetchDataFromPokemonAPI()
+        return () => {
+            alert("Bye bye")
+
+        }
 
     }, [])
+
+    useEffect(() => {
+        fetchDataFromPokemonSearch()
+
+
+    }, [search])
 
     return (
         <div>
             <h1>Pokemon View</h1>
+            <input type="text" onChange={(event) => setSearchCharacter(event.target.value)} placeholder="Type in Pokemon name" />
+            <button type="submit" onClick={() => setSearch(searchCharacters)}>Search</button>
+
+            <img src={searchedPokemon?.sprites?.front_default} alt="error" />
+            <h2>name: {searchedPokemon?.name}</h2>
+            <h2>weight: {searchedPokemon?.weight} lb</h2>
+
             <h2>Here is some examples of pokemons..</h2>
             {pokemonList?.results?.map((pokemon, index) => (
                 <div key={index}>
